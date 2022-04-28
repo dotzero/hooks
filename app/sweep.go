11656528b40a5b090b847c0e7cb15ab454709397
dotzero <mail@dotzero.ru>
2022-04-28 11:55:54 +0300

    Adds infinite loop

--- app/sweep.go
+++ app/sweep.go
@@ -9,20 +9,22 @@ import (
 )
 
 func (a *App) sweep(ctx context.Context) {
-	select {
-	case <-ctx.Done():
-		return
-	case <-time.After(10 * time.Second):
-		log.Printf("[DEBUG] sweep tick")
+	for {
+		select {
+		case <-time.After(10 * time.Second):
+			log.Printf("[DEBUG] sweep tick")
 
-		maxAge := time.Duration(a.BoltTTL) * time.Hour
+			maxAge := time.Duration(a.BoltTTL) * time.Hour
 
-		if err := a.Storage.Sweep(storage.BucketHooks, storage.BucketHooksTTL, maxAge); err != nil {
-			log.Fatalf("[ERROR] failed to sweep hooks, %+v", err)
-		}
+			if err := a.Storage.Sweep(storage.BucketHooks, storage.BucketHooksTTL, maxAge); err != nil {
+				log.Fatalf("[ERROR] failed to sweep hooks, %+v", err)
+			}
 
-		if err := a.Storage.Sweep(storage.BucketReqs, storage.BucketReqsTTL, maxAge); err != nil {
-			log.Fatalf("[ERROR] failed to sweep requests, %+v", err)
+			if err := a.Storage.Sweep(storage.BucketReqs, storage.BucketReqsTTL, maxAge); err != nil {
+				log.Fatalf("[ERROR] failed to sweep requests, %+v", err)
+			}
+		case <-ctx.Done():
+			return
 		}
 	}
 }
